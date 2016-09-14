2690df7d169c5d47950f50afdf8eb2caba79cd64
RPM1984 <rpmir1@gmail.com>
2016-09-14 10:39:57 +1000

    Added ability to set command flags, and batched some commands.

--- Code/InceptionCache.Providers.RedisCacheProvider/RedisCacheExtensions.cs
+++ Code/InceptionCache.Providers.RedisCacheProvider/RedisCacheExtensions.cs
@@ -30,7 +30,7 @@ namespace InceptionCache.Providers.RedisCacheProvider
             {
                 loggingService.Debug($"Cache Hit. Key: {key}");
             }
-            
+
             return serializer.Deserialize<T>(item);
         }
 
@@ -70,7 +70,7 @@ namespace InceptionCache.Providers.RedisCacheProvider
 
             batch.Execute();
 
-            Task.WhenAll(tasks).ConfigureAwait(false);
+            Task.WaitAll(tasks.Cast<Task>().ToArray());
 
             return tasks.Select(result => serializer.Deserialize<T>(result.Result)).ToArray();
         }
@@ -79,20 +79,22 @@ namespace InceptionCache.Providers.RedisCacheProvider
                                                    ISerializer serializer,
                                                    string key,
                                                    T value,
-                                                   TimeSpan? expiry)
+                                                   TimeSpan? expiry,
+                                                   CommandFlags flags = CommandFlags.None)
         {
-            await cache.StringSetAsync(key, serializer.Serialize(value), expiry).ConfigureAwait(false);
+            await cache.StringSetAsync(key, serializer.Serialize(value), expiry, flags: flags).ConfigureAwait(false);
         }
 
         public static async Task AddStringAsync<T>(this IDatabase cache,
                                                    ISerializer serializer,
                                                    Dictionary<string, T> values,
-                                                   TimeSpan? expiry)
+                                                   TimeSpan? expiry,
+                                                   CommandFlags flags = CommandFlags.None)
         {
             var batch = cache.CreateBatch();
 
             var tasks =
-                values.Select(kv => batch.StringSetAsync(kv.Key, serializer.Serialize(kv.Value), expiry)).ToList();
+                values.Select(kv => batch.StringSetAsync(kv.Key, serializer.Serialize(kv.Value), expiry, flags: flags)).ToList();
 
             batch.Execute();
 
@@ -103,64 +105,105 @@ namespace InceptionCache.Providers.RedisCacheProvider
                                         ISerializer serializer,
                                         string key,
                                         T value,
-                                        TimeSpan? expiry)
+                                        TimeSpan? expiry,
+                                        CommandFlags flags = CommandFlags.None)
         {
-            cache.StringSet(key, serializer.Serialize(value), expiry);
+            cache.StringSet(key, serializer.Serialize(value), expiry, flags: flags);
         }
 
         public static void AddString<T>(this IDatabase cache,
                                         ISerializer serializer,
                                         Dictionary<string, T> values,
-                                        TimeSpan? expiry)
+                                        TimeSpan? expiry,
+                                        CommandFlags flags = CommandFlags.None)
         {
             var batch = cache.CreateBatch();
 
-            var tasks =
-                values.Select(kv => batch.StringSetAsync(kv.Key, serializer.Serialize(kv.Value), expiry)).ToList();
+            var tasks = values.Select(kv => batch.StringSetAsync(kv.Key, serializer.Serialize(kv.Value), expiry, flags: flags));
 
             batch.Execute();
 
-            Task.WhenAll(tasks).Wait();
+            Task.WaitAll(tasks.Cast<Task>().ToArray());
         }
 
         public static async Task AddManyToSetAsync<T>(this IDatabase cache,
                                                       ISerializer serializer,
                                                       string key,
                                                       T[] values,
-                                                      TimeSpan? expiry)
+                                                      TimeSpan? expiry,
+                                                      CommandFlags flags = CommandFlags.None)
         {
-            await cache.SetAddAsync(key, GetSerializedRedisValues(serializer, values)).ConfigureAwait(false);
-            await cache.KeyExpireAsync(key, expiry).ConfigureAwait(false);
+            var batch = cache.CreateBatch();
+
+            var tasks = new Task[]
+            {
+                batch.SetAddAsync(key, GetSerializedRedisValues(serializer, values), flags),
+                batch.KeyExpireAsync(key, expiry, flags)
+            };
+
+            batch.Execute();
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
         }
 
         public static async Task AddSingleToSetAsync<T>(this IDatabase cache,
                                                         ISerializer serializer,
                                                         string key,
                                                         T value,
-                                                        TimeSpan? expiry)
+                                                        TimeSpan? expiry,
+                                                        CommandFlags flags = CommandFlags.None)
         {
-            await cache.SetAddAsync(key, serializer.Serialize(value)).ConfigureAwait(false);
-            await cache.KeyExpireAsync(key, expiry).ConfigureAwait(false);
+            var batch = cache.CreateBatch();
+
+            var tasks = new Task[]
+            {
+                batch.SetAddAsync(key, serializer.Serialize(value), flags),
+                batch.KeyExpireAsync(key, expiry, flags)
+            };
+
+            batch.Execute();
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
         }
 
         public static async Task RemoveManyFromSetAsync<T>(this IDatabase cache,
                                                            ISerializer serializer,
                                                            string key,
                                                            T[] values,
-                                                           TimeSpan? expiry)
+                                                           TimeSpan? expiry,
+                                                           CommandFlags flags = CommandFlags.None)
         {
-            await cache.SetRemoveAsync(key, GetSerializedRedisValues(serializer, values)).ConfigureAwait(false);
-            await cache.KeyExpireAsync(key, expiry).ConfigureAwait(false);
+            var batch = cache.CreateBatch();
+
+            var tasks = new Task[]
+            {
+                batch.SetRemoveAsync(key, GetSerializedRedisValues(serializer, values), flags),
+                batch.KeyExpireAsync(key, expiry, flags)
+            };
+
+            batch.Execute();
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
         }
 
         public static async Task RemoveSingleFromSetAsync<T>(this IDatabase cache,
                                                              ISerializer serializer,
                                                              string key,
                                                              T value,
-                                                             TimeSpan? expiry)
+                                                             TimeSpan? expiry,
+                                                             CommandFlags flags = CommandFlags.None)
         {
-            await cache.SetRemoveAsync(key, serializer.Serialize(value)).ConfigureAwait(false);
-            await cache.KeyExpireAsync(key, expiry).ConfigureAwait(false);
+            var batch = cache.CreateBatch();
+
+            var tasks = new Task[]
+            {
+                batch.SetRemoveAsync(key, serializer.Serialize(value), flags),
+                batch.KeyExpireAsync(key, expiry, flags)
+            };
+
+            batch.Execute();
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
         }
 
         public static async Task<T[]> GetSetAsync<T>(this IDatabase cache,
@@ -208,11 +251,12 @@ namespace InceptionCache.Providers.RedisCacheProvider
         public static async Task AddManyToSetsAsync<T>(this IDatabase cache,
                                                        ISerializer serializer,
                                                        Dictionary<string, T[]> keysAndValues,
-                                                       Dictionary<string, TimeSpan?> expiries) where T : class
+                                                       Dictionary<string, TimeSpan?> expiries,
+                                                       CommandFlags flags = CommandFlags.None) where T : class
         {
             var batch = cache.CreateBatch();
             var tasks = keysAndValues
-                .Select(key => batch.SetAddAsync(key.Key, key.Value.Select(value => (RedisValue) serializer.Serialize(value)).ToArray()))
+                .Select(key => batch.SetAddAsync(key.Key, key.Value.Select(value => (RedisValue) serializer.Serialize(value)).ToArray(), flags))
                 .ToList();
             var expireTasks = expiries.Select(key => batch.KeyExpireAsync(key.Key, key.Value)).ToList();
             batch.Execute();
